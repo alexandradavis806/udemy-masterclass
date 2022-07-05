909f70e5601db2bd1605afebb2f388e0c851a4c5
Alexandra Davis <alex.davis806@gmail.com>
2022-07-05 14:59:38 -0400

    addUpTp

---
+++ bigO.js
@@ -0,0 +1,18 @@
+// write a function that calculates the sum of all numbers from 1 up to (and including) some number n
+
+function addUpTo(n) {
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+      total += i;
+    }
+    return total;
+  }
+
+  console.log(addUpTo(6))
+
+
+const addUpToMathy = (n) => {
+    return n * (n + 1) / 2;
+}
+
+console.log(addUpToMathy(6))
