61245494b7a181f34bd1dd4979cfc4afe1ceb7be
Alexandra Davis <alex.davis806@gmail.com>
2022-07-05 15:00:50 -0400

    arrow

--- bigO.js
+++ bigO.js
@@ -1,6 +1,6 @@
 // write a function that calculates the sum of all numbers from 1 up to (and including) some number n
 
-function addUpTo(n) {
+const addUpTo = (n) => {
     let total = 0;
     for (let i = 1; i <= n; i++) {
       total += i;
