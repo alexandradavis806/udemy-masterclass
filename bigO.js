73982d9bdef9ef03096df36e1e945d79319b6af8
Alexandra Davis <alex.davis806@gmail.com>
2022-07-11 19:05:33 -0400

    new probs

--- bigO.js
+++ bigO.js
@@ -22,3 +22,32 @@ const addUpToMathy = (n) => {
 
 
 console.log(addUpToMathy(6))
+
+
+// const charCount = (str) => {
+//     const obj = {}
+//     for (let char of str) {
+//         char = char.toLowerCase();
+//         if (/[a-z0-9]/.test(char)) {
+//             if (obj[char] > 0) {
+//                 obj[char]++;
+//             } else {
+//                 obj[char] = 1;
+//             }
+//         }
+//     }
+//     return obj
+// }
+
+const charCount = (str) => {
+    const obj = {}
+    for (let char of str) {
+        char = char.toLowerCase();
+        if (/[a-z0-9]/.test(char)) {
+            obj[char] = ++obj[char] || 1
+        }
+    }
+    return obj
+}
+
+console.log(charCount("Hello hi!"))
