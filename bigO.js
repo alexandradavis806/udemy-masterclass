be5c2d1286faae0e42f52996859c37f60ae300b5
Alexandra Davis <alex.davis806@gmail.com>
2022-07-11 19:11:19 -0400

    problems

--- bigO.js
+++ bigO.js
@@ -39,11 +39,37 @@ console.log(addUpToMathy(6))
 //     return obj
 // }
 
+// const charCount = (str) => {
+//     const obj = {}
+//     for (let char of str) {
+//         char = char.toLowerCase();
+//         if (/[a-z0-9]/.test(char)) {
+//             obj[char] = ++obj[char] || 1
+//         }
+//     }
+//     return obj
+// }
+
+// console.log(charCount("Hello hi!"))
+
+
+ // using character codes are WAY faster than regular expressions:
+
+const isAlphaNumeric = char => {
+    let code = char.charCodeAt(0);
+    if (!(code > 47 && code < 58) &&
+        !(code > 64 && code < 91) &&
+        !(code > 96 && code < 123)) {
+            return false
+        }
+        return true
+}
+
 const charCount = (str) => {
     const obj = {}
     for (let char of str) {
         char = char.toLowerCase();
-        if (/[a-z0-9]/.test(char)) {
+        if (isAlphaNumeric(char)) {
             obj[char] = ++obj[char] || 1
         }
     }
