6a6aa4c0e20c070b4d96536527349cb3f927b7bf
Alexandra Davis <alex.davis806@gmail.com>
2022-07-13 13:37:03 -0400

    multi pointers

--- MultiplePointers.js
+++ MultiplePointers.js
@@ -38,3 +38,23 @@ const sumZero = (arr) => {
 }
 
 console.log(sumZero([-4, -3, -2, -1, 0, 1]))
+
+
+// implement a function which accepts a sorted array, and counts the unique values in the array. there can be negative numbers in the array, but it will always be sorted
+
+const countUniqueValues = (arr) => {
+    if(arr.length === 0) {
+        return 0
+    }
+    let i = 0 
+    for (let j = 1; j < arr.length; j++){
+        if(arr[i] !== arr[j]){
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1
+}
+
+console.log(countUniqueValues([1,1,2,3,3,4,5,6,6,7]))
+console.log(countUniqueValues([]))
