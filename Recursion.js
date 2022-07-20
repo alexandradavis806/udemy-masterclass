3a01c69ab0b0e1894a68287689010fc47d47e95b
Alexandra Davis <alex.davis806@gmail.com>
2022-07-20 14:07:56 -0400

    recursion

--- Recursion.js
+++ Recursion.js
@@ -44,3 +44,17 @@ const sumRange = (num) => {
 
 console.log(sumRange(3))
 
+const factorial = num => {
+    let total = 1;
+    for(let i = num; i > 1; i--){
+        total *= i
+    }
+    return total;
+}
+
+const recursiveFactorial = num => {
+    if(num === 1) return 1;
+    return num + recursiveFactorial(num - 1)
+}
+
+console.log(recursiveFactorial(3))
